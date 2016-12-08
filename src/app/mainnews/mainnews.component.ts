a5934e86a9e8f5646fb41215171a3f9c2238f437
Vijayan PP <vijayan.pp@pitsolutions.com>
2016-12-08 17:29:20 +0530

    'vijaan'

--- src/app/mainnews/mainnews.component.ts
+++ src/app/mainnews/mainnews.component.ts
@@ -18,10 +18,23 @@ public selectNews;
   	 {
      this.NewsContent=news;
      this.News=this.NewsContent.articles;
+     });
+
+    this.todayNews.retrieveNews('https://newsapi.org/v1/articles?source=entertainment-weekly&sortBy=top&apiKey=3e22f2fcc1344975ae2b2e69379e2a6e').subscribe(news=>
+     {
+     this.NewsContent=news;
+     this.News=this.News.concat(this.NewsContent.articles);
      for(let i=0;i<this.News.length;i++)
      {
       this.News[i].source=this.NewsContent.source;
-     }});
+     }
+     });
+
+     /*console.log(this.News.length);
+     for(let i=0;i<this.News.length;i++)
+     {
+      this.News[i].source=this.NewsContent.source;
+     }*/
   }
   
 
