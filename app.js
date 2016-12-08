775ece963f075fedf23625258c2eb5f6556d7b17
Vijayan PP <vijayan.pp@pitsolutions.com>
2016-12-08 19:38:20 +0530

    'vijay'

---
+++ app.js
@@ -0,0 +1,24 @@
+var express = require('express');
+var app = express();
+
+
+app.set('port', (process.env.PORT || 5000));
+
+app.use(express.static(__dirname + '/dist'));
+
+
+app.get('/', function(request, response) {
+  response.render('index.html');
+});
+
+app.get('/news', function(request, response) {
+  response.send('helo world')
+})
+
+
+app.listen(app.get('port'), function() {
+  console.log('Node app is running on port', app.get('port'));
+});
+
+
+
